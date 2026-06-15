27d8a6883acd975882424fed7269b7c159c93d5a
tihomirov <antontikhomirov93@gmail.com>
2026-06-15 11:25:22 +0300

    chore: refactor scss files

--- src/shared/api/weather/weatherService.ts
+++ src/shared/api/weather/weatherService.ts
@@ -2,11 +2,13 @@ import { createOpenMeteoProvider } from './providers/open-meteo-provider/openMet
 import { createWeatherApiProvider } from './providers/weather-api-provider/weatherApiProvider';
 import { createWeatherFacade } from './weatherFacade';
 
+const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
 export const weatherService = createWeatherFacade(
   [
     createOpenMeteoProvider(), // main provider
-    createWeatherApiProvider({ // fallback provider
-      apiKey: import.meta.env.VITE_WEATHER_API_KEY,
+    WEATHER_API_KEY && createWeatherApiProvider({ // fallback provider if API key is added
+      apiKey: WEATHER_API_KEY,
     }),
-  ],
+  ].filter(Boolean),
 );
